3fd606608c4545dd528c3ad6a177c0f674249d42
karthikkone <kirti.annajigar@ad.infosys.com>
2019-01-18 17:35:56 +0530

    Create AuthDataRepository.java

---
+++ src/main/java/com/jenkinsjobs/Jobs/AuthDataRepository.java
@@ -0,0 +1,8 @@
+package com.jenkinsjobs.Jobs;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthDataRepository extends JpaRepository<JobStatus,Long>{
+}
