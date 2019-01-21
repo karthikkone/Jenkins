bb0b2ba5bb1d48ea6332875cc882fbb0da9f3bcd
karthikkone <kirti.annajigar@ad.infosys.com>
2019-01-21 16:02:15 +0530

    Update FetchJobs.java

--- src/main/java/com/jenkinsjobs/Jobs/FetchJobs.java
+++ src/main/java/com/jenkinsjobs/Jobs/FetchJobs.java
@@ -14,12 +14,16 @@ import java.util.Map;
 
 import javax.print.attribute.standard.JobState;
 
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
 
 import net.sf.json.*;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -56,22 +60,20 @@ public class FetchJobs {
     boolean flag=false;
     private static QueueReference queueRef;
     private static QueueItem queueItem;
-    private AuthDataRepository  authDataRepository;
+    //private AuthDataRepository  authDataRepository;
+    private static SessionFactory sessionFactory;
+    private static Session session;
 	public FetchJobs()
 	{
-		
+		sessionFactory = new Configuration().configure().buildSessionFactory();
 	}
-	@Autowired
-    public FetchJobs(AuthDataRepository  authDataRepository) {
-        this.authDataRepository = authDataRepository;     
-    }
+	
 	@RequestMapping(value="/jobs", method=RequestMethod.GET)
 	public JSONObject getJobs() throws Exception 
 	{
 		 try {
 	         //jenkins = new JenkinsServer(new URI("https://kone.iagilepro.com"), "agilepro", "infosys@123");
-		 //jenkins = new JenkinsServer(new URI("http://localhost:8080/"), "kit", "kit");
-		 jenkins = new JenkinsServer(new URI("https://kone.iagilepro.com"), "agile.pro@kone.com", "infy1234");
+			 jenkins = new JenkinsServer(new URI("http://localhost:8080/"), "kit", "kit");
 	         List<String> jobnames = new ArrayList<String>();    
 	         Map<String, Job> jobs = jenkins.getJobs();
 	         //System.out.println("new jobs... :"+jobs);
@@ -98,146 +100,53 @@ public class FetchJobs {
 	public JSONObject StartJob(@RequestParam("buildname") String buildname) throws Exception 
 	//public void StartJob(String buildname) throws Exception
 	{
-<<<<<<< HEAD
-		 JSONObject jsonobj = new JSONObject();	       
+		String status;		
+		JSONObject jsonobj = new JSONObject();	       
 		JobStatus jobStat = new JobStatus();
 		jobStat.setBuildname(buildname);
 		jobStat.setBuildstatus("In Progress");
-		System.out.println("buildname :"+jobStat.getBuildname());
-		authDataRepository.saveAndFlush(jobStat);
-		Thread bThread = new Thread(new BuildThread(jobStat.getBuildid(),buildname));
-	    bThread.start();
-	    jsonobj.put("result", "success");
+		System.out.println("buildname :"+jobStat.getBuildname());		
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(jobStat);       
+		session.getTransaction().commit();
+		List<JobStatus> jobs1 = session.createQuery("FROM JobStatus").list();
+        //jobs.forEach((x) -> System.out.printf("- %s%n", x));        
+		jsonobj.put("Build id", jobStat.getBuildid());
+		jsonobj.put("Build Name", jobStat.getBuildname());
+		jsonobj.put("Build Status", jobStat.getBuildstatus());
+        for(int i=0;i<jobs1.size();i++)
+        {
+        	System.err.println("jobs in array :"+jobs1.get(i).getBuildid()+" "+jobs1.get(i).getBuildname()+" "+jobs1.get(i).getBuildstatus());
+        }
+		BuildThread b= new BuildThread(jobStat.getBuildid(),buildname);
+		status=b.Start(sessionFactory);
+		jobStat.setBuildstatus(status);
+		List<JobStatus> jobs = session.createQuery("FROM JobStatus").list();
+        //jobs.forEach((x) -> System.out.printf("- %s%n", x));          
+        for(int i=0;i<jobs.size();i++)
+        {
+        	System.err.println("jobs in array :"+jobs.get(i).getBuildid()+" "+jobs.get(i).getBuildname()+" "+jobs.get(i).getBuildstatus());
+        }	    
 		return jsonobj;
-=======
-		String res = null;
-		try{
-		//jenkins = new JenkinsServer(new URI("https://kone.iagilepro.com"), "agile.pro@kone.com", "infy@1234");
-		//jenkins = new JenkinsServer(new URI("http://localhost:8080/"), "kit", "kit");		
-		jenkins = new JenkinsServer(new URI("https://kone.iagilepro.com"), "agile.pro@kone.com", "infy1234");
-		JSONObject jsonobj = new JSONObject();	
-		JobWithDetails jobinfo = jenkins.getJob(buildname);		
-		//queueRef=jobinfo.build(true);		
-		//queueItem = jenkins.getQueueItem(queueRef);
-		 ExecutorService executor = Executors.newFixedThreadPool(1);
-		 List<Future<String>> list = new ArrayList<Future<String>>();
-		 Callable<String> callable = new MyCallable(jenkins,buildname);
-		 Future<String> future = executor.submit(callable);
-		 list.add(future);
-		 for(Future<String> fut : list){
-	            try {
-	                //print the return value of Future, notice the output delay in console
-	                // because Future.get() waits for task to get completed
-	                //System.out.println(new Date()+ "::"+fut.get());
-			jsonobj.put("Result :",fut.get());
-	            	//System.out.println(new Date()+ "::"+future);
-	            } 
-	            catch (Exception e) {
-	                e.printStackTrace();
-	            }
-		 }
-		 return jsonobj;
-		/*JSONObject jsonobj = new JSONObject();				
-		while (queueItem.getExecutable() == null) {		
-		       Thread.sleep(DEFAULT_RETRY_INTERVAL);
-		       queueItem = jenkins.getQueueItem(queueRef);
-		      
-		}
-		Build build = jenkins.getBuild(queueItem);	
-		System.out.println("queue item 2:"+queueItem);		
-		//jsonobj.put("Id", build.getQueueId());		
-		/*while(CheckStatus(queueItem) == true)
-		{
-			System.out.println("Job Is In Progress");
-		}
-		if(CheckResult(queueItem)!=null)
-		{
-		if(build.details().getResult() == build.details().getResult().SUCCESS)
-		{res="success";	}
-		
-		return res;*/
-		}
-		 catch (Exception e) {
-	         System.err.println(e.getMessage());
-	         throw e;
-	     	}
-		finally 
-		{
-		jenkins.close();
-		}
->>>>>>> 7d3f7d86a7f3bd5f8863252696200f3836a07385
 	}
-	@RequestMapping(value="/CheckStatus",params={"queueid"},method=RequestMethod.GET)	
-	public JSONObject CheckStatus(@RequestParam("queueid") QueueItem queueid) throws Exception 
-	//public boolean CheckStatus(String q)
+	@RequestMapping(value="/CheckStatus",params={"buildid"},method=RequestMethod.GET)	
+	public JSONObject CheckStatus(@RequestParam("buildid") QueueItem queueid) throws Exception 
+	//public List<JobStatus> CheckStatus(long buildid)
 	{
-	    Build build;
-	    JSONObject jsonobj = new JSONObject();	
+	    
 		try {
-			jenkins = new JenkinsServer(new URI("http://localhost:8080/"), "kit", "kit");			
-			build = jenkins.getBuild(queueid);
-			while(build.details().isBuilding() == true)
-			{						 
-				//Thread.sleep(DEFAULT_RETRY_INTERVAL);
-				 //System.out.println(Building());
-				//return build.details().isBuilding();
-				jsonobj.put("Status:","In Progress");	
-				return jsonobj;
-			}
-			System.out.println("Queue item in check ,,..."+build.details().getResult());
-		} catch (URISyntaxException e) {
+			JSONObject Jsonobj=new JSONObject();
+			List<JobStatus> jobs1 = session.createQuery("FROM JobStatus where buildid="+buildid).list();
+			Jsonobj.put("Result", jobs1.get(1).getBuildstatus());
+			return Jsonobj;
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}							
-		 catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		catch (Exception e) {
-	         System.err.println(e.getMessage());
-	         
-	     	}		
-		jsonobj.put("Status:", "null");	
-		return jsonobj;
-	}
-	@RequestMapping(value="/CheckResult",params={"queueid"},method=RequestMethod.GET)	
-	public JSONObject CheckResult(@RequestParam("queueid") QueueItem queueid) throws Exception 
-	//public String CheckResult(QueueItem q)
-	{
-
-	    Build build;
-	    JSONObject jsonobj = new JSONObject();	
-		try {
-			jenkins = new JenkinsServer(new URI("http://localhost:8080/"), "kit", "kit");
-			build = jenkins.getBuild(queueid);
-			if(build.details().getResult() == build.details().getResult().SUCCESS)
-			{						 
-				//Thread.sleep(DEFAULT_RETRY_INTERVAL);
-				 //System.out.println("Last build :"+build.details().get);
-				//return "Successfully completed";
-				jsonobj.put("Result:", "Successfully completed");	
-				return jsonobj;
-			}
-			else if (build.details().getResult() == build.details().getResult().FAILURE) {
-				jsonobj.put("Result:", "Failed");	
-				return jsonobj;
-			}
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}							
-		 catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		catch (Exception e) {
-	         System.err.println(e.getMessage());
-	         
-	     	}	
-		
-		jsonobj.put("Result:", "error");	
-		return jsonobj;
-	}
+		return null;
+	}	
+			
 	@RequestMapping(value="/Stopjobs",method=RequestMethod.GET)
 	public JSONObject StopJob() throws Exception 
 	{
