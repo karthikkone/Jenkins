f89db16b8324dedae48dd872c02e0897d5ce168e
karthikkone <kirti.annajigar@ad.infosys.com>
2019-01-21 16:02:10 +0530

    Update BuildThread.java

--- src/main/java/com/jenkinsjobs/Jobs/BuildThread.java
+++ src/main/java/com/jenkinsjobs/Jobs/BuildThread.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
+//import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.offbytwo.jenkins.JenkinsServer;
@@ -20,14 +20,13 @@ import com.offbytwo.jenkins.model.QueueReference;
 
 import net.sf.json.JSONObject;
 
-public class BuildThread implements Runnable{
+public class BuildThread{
 	
 	private String buildName;
 	private Long buildId;
 	private static final Long DEFAULT_RETRY_INTERVAL = 200L;
 	private static QueueReference queueRef;
 	private static QueueItem queueItem;	 
-	private AuthDataRepository  authDataRepository;
 	public BuildThread()
 	{
 		
@@ -35,88 +34,12 @@ public class BuildThread implements Runnable{
 	public BuildThread(long buildId,String buildName) {
 		this.buildId = buildId;
 		this.buildName = buildName;
-	}
-	@Autowired
-    public BuildThread(AuthDataRepository  authDataRepository) {
-        this.authDataRepository = authDataRepository;     
-    }
-	/*public void ConnectDB()
-	{
-		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		
-	}*/
-	private static void CheckBuildStatus(SessionFactory sessionFactory, long buildId) {
-		JSONObject jsonobj = new JSONObject();	
-        System.out.println("-- loading Jobs --");
-        Session session = sessionFactory.openSession();
-        @SuppressWarnings("unchecked")
-        List<JobStatus> jobs = session.createQuery("FROM JobStatus WHERE buildid= :buildId")
-        .setParameter("buildId", buildId)
-        .list();
-        
-        //jobs.forEach((x) -> System.out.printf("- %s%n", x));          
-        for(int i=0;i<jobs.size();i++)
-        {
-        	System.err.println("jobs in array :"+jobs.get(i).getBuildname()+" "+jobs.get(i).getBuildstatus());
-        }
-       // session.close();
-    }
-    private static void CreateBuildInDB(SessionFactory sessionFactory,long buildId,String BuildName,String BuildStatus) {
-    	Session session = sessionFactory.openSession();
-    	try {
-    	JobStatus s1 = new JobStatus(buildId, BuildName,BuildStatus);        
-        System.out.println("-- persisting jobs --");    
-        session.beginTransaction();
-        session.save(s1);       
-        session.getTransaction().commit();
-        //load(sessionFactory);
-        CheckBuildStatus(sessionFactory, buildId);
-    	}
-    	catch(Exception e)
-    	{
-    		System.out.println(e.getMessage());
-    	}
-    	/*finally
-    	{
-    		session.close();
-    	}*/
-    }
-    private void UpdateBuildInDB(long BuildId,String BuildStatus)
-    { 	
-    	
-    	System.out.println("updating build  :"+BuildId+" status "+BuildStatus);
-        /*Query updated = session.createQuery("update JobStatus SET BuildStatus =:buildStatus WHERE buildid =:buildId")
-        		.setParameter("buildStatus", BuilsStatus)
-        		.setParameter("buildId", BuildId);*/
-    	//Query updated = session.createQuery("update JobStatus SET buildstatus ="+"'"+BuildStatus+"'"+" WHERE buildid ="+BuildId);
-    	
-    	if(authDataRepository.findById(buildId).isPresent());
-    	{
-    		JobStatus updatedjobststaus=new JobStatus();
-    		updatedjobststaus.setBuildstatus(BuildStatus);
-		    authDataRepository.save(updatedjobststaus);
-    	}
-        System.out.println("value of updated var :"+authDataRepository.findAll());
-        //update
-    	/*int no=updated.executeUpdate();
-    	System.out.println("no of records updated :"+no);
-    	session.getTransaction().commit();
-    	
-    	@SuppressWarnings("unchecked")
-    	List<JobStatus> jobs = session.createQuery("FROM JobStatus WHERE buildid=:buildId")
-    			.setParameter("buildId", BuildId).list();
-    	
-    	for(int i=0;i<jobs.size();i++)
-        {
-    	System.out.println("final result :"+jobs.get(i).getBuildname()+" "+jobs.get(i).getBuildstatus());
-        }
-    	session.close();*/
-    }
-	@Override
-	public void run() {
+	} 
+	
+	public String Start(SessionFactory s) {
 		// TODO Auto-generated method stub
 		JenkinsServer jenkins;
-		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		SessionFactory sessionFactory = s;
 		try {
 			jenkins = new JenkinsServer(new URI("http://localhost:8080/"), "kit", "kit");			 
 			 JobWithDetails jobinfo = jenkins.getJob(this.buildName);
@@ -136,17 +59,17 @@ public class BuildThread implements Runnable{
 			}
 			if(build.details().getResult() == build.details().getResult().SUCCESS)
 			{						 
-				UpdateBuildInDB(this.buildId,"Success");
+				return "Successfully Completed";
 			}
 			else if (build.details().getResult() == build.details().getResult().FAILURE) {
-				UpdateBuildInDB(this.buildId,"Failure");
+				return "build Failed";
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}		
 			
-		
+		return null;
 		
 	}
 	
