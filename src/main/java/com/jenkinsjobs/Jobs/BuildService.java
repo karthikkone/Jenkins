68fdfac2aae85e3c1f796ca46cf655e5f0d97c75
karthikkone <kirti.annajigar@ad.infosys.com>
2019-01-28 15:14:40 +0530

    Create BuildService.java

---
+++ src/main/java/com/jenkinsjobs/Jobs/BuildService.java
@@ -0,0 +1,8 @@
+package com.jenkinsjobs.Jobs;
+
+public interface BuildService {
+    JobStatus createBuild(JobStatus job);
+    JobStatus getbuild(Long buildid);
+    JobStatus updateBuild(JobStatus job);
+    long getCount();
+}
