5e4f90ee35598c4d620a650e0016d9ca8d5327e4
karthikkone <kirti.annajigar@ad.infosys.com>
2019-01-28 11:12:54 +0530

    Update BuildThread.java

--- src/main/java/com/jenkinsjobs/Jobs/BuildThread.java
+++ src/main/java/com/jenkinsjobs/Jobs/BuildThread.java
@@ -6,9 +6,12 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
+import javax.management.Query;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.NativeQuery;
 //import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -27,6 +30,7 @@ public class BuildThread{
 	private static final Long DEFAULT_RETRY_INTERVAL = 200L;
 	private static QueueReference queueRef;
 	private static QueueItem queueItem;	 
+	private static Session session;
 	public BuildThread()
 	{
 		
@@ -36,14 +40,15 @@ public class BuildThread{
 		this.buildName = buildName;
 	} 
 	
-	public String Start(SessionFactory s) {
+	public void Start(SessionFactory s) {
 		// TODO Auto-generated method stub
 		JenkinsServer jenkins;
 		SessionFactory sessionFactory = s;
 		try {
-			
+			jenkins = new JenkinsServer(new URI("http://localhost:8080/"), "kit", "kit");			 
 			 JobWithDetails jobinfo = jenkins.getJob(this.buildName);
 			queueRef=jobinfo.build(true);	
+			session = sessionFactory.openSession();
 			//CreateBuildInDB(sessionFactory,1,this.buildName,"In Progress");
 		    queueItem = jenkins.getQueueItem(queueRef);
 		    JSONObject jsonobj = new JSONObject();				
@@ -52,25 +57,64 @@ public class BuildThread{
 			       queueItem = jenkins.getQueueItem(queueRef);
 			      
 			}
-			Build build = jenkins.getBuild(queueItem);	
+			Build build = jenkins.getBuild(queueItem);				
 			while(build.details().isBuilding() == true)
 			{						 
 				continue;
 			}
+			
 			if(build.details().getResult() == build.details().getResult().SUCCESS)
-			{						 
-				return "Successfully Completed";
+			{	
+				System.out.println("inside success");
+				String Updatequery= "UPDATE JobStatus set buildstatus = :buildstatus "+"WHERE buildid = :buildid";
+				System.out.println("value in updatequry :"+Updatequery);
+				org.hibernate.query.Query Update= session.createQuery(Updatequery);
+				Update.setParameter("buildstatus", "Successfully Completed");
+				Update.setParameter("buildid", this.buildId);
+				session.beginTransaction();
+				int result = Update.executeUpdate();
+				session.getTransaction().commit();
+				System.out.println("Rows affected: " + result);
+				List<JobStatus> jobs1 = session.createQuery("FROM JobStatus where buildid="+this.buildId).list();
+			    System.out.println("result :"+jobs1.get(0).getBuildstatus());
+				//Jsonobj.put("Result", jobs1.get(i).getBuildstatus());
+		       
+				//return "Successfully Completed";
 			}
 			else if (build.details().getResult() == build.details().getResult().FAILURE) {
-				return "build Failed";
+				System.out.println("inside Failed");
+				org.hibernate.query.Query Update=session.createQuery("Update JobStatus set buildstatus="+"build Failed"+" where buildid="+this.buildId);
+				Update.executeUpdate();
+				//return "build Failed";
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}		
 			
-		return null;
+		//return null;
 		
 	}
+	/*public List<JobStatus> CheckStatus(SessionFactory s,long buildid)
+	{
+	    
+		try {
+			JSONObject Jsonobj=new JSONObject();
+			SessionFactory sessionFactory = s;
+			session = sessionFactory.openSession();
+			List<JobStatus> jobs1 = session.createQuery("FROM JobStatus where buildid="+buildid).list();
+			for(int i=0;i<jobs1.size();i++)
+	        {
+				System.out.println("result :"+jobs1.get(i).getBuildstatus());
+			//Jsonobj.put("Result", jobs1.get(i).getBuildstatus());
+	        }
+			//return Jsonobj;
+			return jobs1;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}							
+		return null;
+	}	*/
 	
 }
