30095906f67c5dfb4d748ff976b18abdff817a48
karthikkone <kirti.annajigar@ad.infosys.com>
2019-01-28 11:13:11 +0530

    Update JobStatus.java

--- src/main/java/com/jenkinsjobs/Jobs/JobStatus.java
+++ src/main/java/com/jenkinsjobs/Jobs/JobStatus.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+
 public class JobStatus {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -31,6 +32,41 @@ public class JobStatus {
 	}
 	public String getBuildname() {
 		return buildname;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((buildid == null) ? 0 : buildid.hashCode());
+		result = prime * result + ((buildname == null) ? 0 : buildname.hashCode());
+		result = prime * result + ((buildstatus == null) ? 0 : buildstatus.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		JobStatus other = (JobStatus) obj;
+		if (buildid == null) {
+			if (other.buildid != null)
+				return false;
+		} else if (!buildid.equals(other.buildid))
+			return false;
+		if (buildname == null) {
+			if (other.buildname != null)
+				return false;
+		} else if (!buildname.equals(other.buildname))
+			return false;
+		if (buildstatus == null) {
+			if (other.buildstatus != null)
+				return false;
+		} else if (!buildstatus.equals(other.buildstatus))
+			return false;
+		return true;
 	}
 	public void setBuildname(String buildname) {
 		this.buildname = buildname;
