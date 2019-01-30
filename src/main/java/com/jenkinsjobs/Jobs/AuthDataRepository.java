06d8466fb2d50f81e4aebae8bdaf86bc94edbdfa
karthikkone <karthik.andhiyur@kone.com>
2019-01-30 12:37:17 +0530

    Delete AuthDataRepository.java

--- src/main/java/com/jenkinsjobs/Jobs/AuthDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jenkinsjobs.Jobs;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthDataRepository extends JpaRepository<JobStatus,Long>{
-}
-
