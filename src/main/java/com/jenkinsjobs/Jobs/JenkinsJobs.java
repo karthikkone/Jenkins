762c8a30a91e155f432c727fe9e44bf0528cf597
karthikkone <karthik.andhiyur@kone.com>
2019-03-13 18:51:15 +0530

    Update JenkinsJobs.java

--- src/main/java/com/jenkinsjobs/Jobs/JenkinsJobs.java
+++ src/main/java/com/jenkinsjobs/Jobs/JenkinsJobs.java
@@ -132,7 +132,7 @@ public class JenkinsJobs {
 	             //{
 	            	// Node ParamType1 = ParamTypes.item(k);
 	            	//Node ParamType1 = ParamTypes.item(0);
-	            	if(ParamType != null && ParamType.getNodeType() == ParamType.ELEMENT_NODE)
+	            	if(ParamType != null)
 	            	{
 			//Paramtypes.add(ParamType.getNodeName());
 			 //Paramtypes.put("Paramtype",ParamType.getNodeName());
